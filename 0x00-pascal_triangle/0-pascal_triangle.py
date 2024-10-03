f4ddbd500190d1db0a652fd726ad41983bf5e75c
Abdulnasir Yakubu muhammad <132953212+Abdulnasirym@users.noreply.github.com>
2024-10-03 13:54:58 +0100

    pascal's triangle code commenting

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,27 +3,19 @@
 Pascal's Triangle
 """
 
+
 def pascal_triangle(n):
-    """Create a function that returns a list of lists
-    of integers representing Pascal’s triangle of n rows.
-    
-    Args:
-        n (int): The number of rows in Pascal's triangle.
-
-    Returns:
-        list: A list of lists representing Pascal's triangle.
+    """Create a function def pascal_triangle(n): that returns a list of lists
+    of integers representing the Pascal’s triangle of n
     """
-    if n <= 0:
-        return []  # Return empty list for non-positive integers
-
-    triangle = [[1]]  # Initialize the triangle with the first row
-
+    triangle = [[1]]
+    if n <= 0 or type(n) is not int:
+        return []
     for i in range(1, n):
-        row = [1]  # Start each row with a 1
+        row = [1]
+        
         for j in range(1, i):
-            # Each value is the sum of the two values above it
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)  # End each row with a 1
-        triangle.append(row)  # Add the completed row to the triangle
-
+        row.append(1)
+        triangle.append(row)
     return triangle
