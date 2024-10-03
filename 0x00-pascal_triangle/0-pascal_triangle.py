a12e91610bb515f2777f810947c0ea3b5a15e1e0
Abdulnasir Yakubu muhammad <132953212+Abdulnasirym@users.noreply.github.com>
2024-10-03 19:11:40 +0100

    adjusted spaces

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,9 +3,7 @@
 
 
 def pascal_triangle(n):
-    """Create a function def pascal_triangle(n): that returns a list of lists
-    of integers representing the Pascal’s triangle of n
-    """
+    """Function that creates a pascal's triangle of size n"""
     if n <= 0:
         return []
     triangle = [[1]]
@@ -14,5 +12,5 @@ def pascal_triangle(n):
         for j in range(1, i):
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         row.append(1)
-        triangle.append(row)
+    triangle.append(row)
     return triangle
