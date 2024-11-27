628f3e81481676ca70bba1fe366454d1aaf27d11
abdulnasirym <abdulnasiry05@gmail.com>
2024-11-27 16:06:25 +0000

    a function that makes chnages

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" A module that make changes based on a given coin """
+
+
+def makeChange(coins, total):
+    """ The function that make the changes """
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
